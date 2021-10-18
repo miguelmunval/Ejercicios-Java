92e6b2da8b9b297cbb771bc1f309b4d8534cee41
Miguel Ángel Muñoz Valenzuela <91052453+miguelmunval@users.noreply.github.com>
2021-10-18 12:15:18 +0200

    Add files via upload

---
+++ tabla.java
@@ -0,0 +1,13 @@
+public class tabla {
+  public static void main (String[] args){
+    System.out.println("____________________________________________________");
+    System.out.printf("%10s\n","| Lunes  | Martes  | Miercoles  | Jueves  | Viernes|");
+    System.out.printf("%10s\n","| LM     | ED      | LM         | ED      | FOL    |");
+    System.out.printf("%10s\n","| LM     | BBDD    | LM         | ED      | FOL    |");
+    System.out.printf("%10s\n","| PROG   | BBDD    | PROG       | PROG    | FOL    |");
+    System.out.printf("%10s\n","| PROG   | PROG    | PROG       | PROG    | SINF   |");
+    System.out.printf("%10s\n","| BBDD   | PROG    | BBDD       | SINF    | SINF   |");
+    System.out.printf("%10s\n","| BBDD   | SINF    | BBDD       | SINF    | SINF   |");
+    System.out.println("|________|_________|____________|_________|________|");
+    }
+  }
