11fa3506c190b4893d18862ae92f7b1633a542cb
miguelmunval <miguelangel.munoz.fp@iescampanillas.com>
2021-10-20 10:54:36 +0200

    actualizacion

---
+++ t1/piramide.java
@@ -0,0 +1,10 @@
+public class piramide {
+  public static void main (String[] args){
+    System.out.print("*********");
+    System.out.println();
+    System.out.printf("%2s%6s\n","*","*");
+    System.out.printf("%3s%4s\n","*","*");
+    System.out.printf("%4s%2s\n","*","*");
+    System.out.printf("%5s\n","*");
+    }
+  }
