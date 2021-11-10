cc2c90ff23deddc84e921e3bc98bc379431b0204
Miguel Ángel Muñoz Valenzuela <91052453+miguelmunval@users.noreply.github.com>
2021-11-10 23:16:38 +0100

    Add files via upload

--- T6/ejerciciodeclase.java
+++ T6/ejerciciodeclase.java
@@ -1,21 +1,30 @@
 public class ejerciciodeclase {
   public static void main(String[] args) {
-    int numero;
-		System.out.print("Introduzca un número impar: ");
-		numero=Integer.parseInt(System.console().readLine());
-		for (int i = 0; i < numero; i++) {
-			for (int j = 0; j < numero; j++) {
-				if (((i==0)||(i==9))||(((i>=1)&&(i<=numero-1)) && (j==0))) {
-					System.out.print(numero/2+1+" ");
-				}
-				if (((i==1)&&(j>0)&&(j<numero-1))||(((i>=2)||(i<numero-1))&&((j==0)||(j==numero-2)))) {
-					System.out.print(numero/2+" ");
-				}
-				if (((i==2)||(i==numero-3))&&((j>1)&&(j<numero-2))) {
-					System.out.print(numero/2-1+" ");
-				}
-			}
-			System.out.println();
-		}
-	}
+    int size, mitad, minimo, numeroPintado;
+
+    System.out.print("Introduzca el tamaño del anillo (impar): ");
+    size = Integer.parseInt(System.console().readLine());
+
+    System.out.println();
+
+    mitad = size / 2 + 1;
+
+    for (int i = 0; i < size; i++) {
+      numeroPintado = mitad;
+
+      for (int j = 1; j <= size; j++) {
+        System.out.print(numeroPintado + " ");
+
+        if (i + 1 > mitad) minimo = i - mitad + 2;
+        else minimo = mitad - i;
+
+
+        if (numeroPintado > minimo && j < mitad) numeroPintado--;
+        else if (i + 1 > mitad) {
+          if (j >= size - (mitad - minimo)) numeroPintado++;
+        } else if (j >= size - i) numeroPintado++;
+      }
+      System.out.println();
+    }
+  }
 }
